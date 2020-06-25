a02cf9a5000fbfd21203f8cd5666fa5add5b6470
MakkiAnjum <muhammadmakkianjum@yahoo.om>
2020-06-25 17:52:35 +0500

    Added backend to post and get data

--- app/navigation/NewListingButton.js
+++ app/navigation/NewListingButton.js
@@ -5,31 +5,31 @@ import { MaterialCommunityIcons } from "@expo/vector-icons";
 import colors from "../config/colors";
 
 function NewListingButton({ onPress }) {
-    return (
-        <TouchableOpacity onPress={onPress}>
-            <View style={styles.container}>
-                <MaterialCommunityIcons
-                    name="plus-circle"
-                    color={colors.white}
-                    size={40}
-                />
-            </View>
-        </TouchableOpacity>
-    );
+  return (
+    <TouchableOpacity onPress={onPress}>
+      <View style={styles.container}>
+        <MaterialCommunityIcons
+          name="plus-circle"
+          color={colors.white}
+          size={40}
+        />
+      </View>
+    </TouchableOpacity>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        alignItems: "center",
-        backgroundColor: colors.primary,
-        borderColor: colors.white,
-        borderRadius: 40,
-        borderWidth: 10,
-        bottom: 20,
-        height: 80,
-        justifyContent: "center",
-        width: 80,
-    },
+  container: {
+    alignItems: "center",
+    backgroundColor: colors.primary,
+    borderColor: colors.white,
+    borderRadius: 40,
+    borderWidth: 10,
+    bottom: 20,
+    height: 80,
+    justifyContent: "center",
+    width: 80,
+  },
 });
 
 export default NewListingButton;
