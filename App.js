add962e5290390ebdece94bdc5be6e16620183cd
MakkiAnjum <muhammadmakkianjum@yahoo.om>
2020-06-26 12:10:27 +0500

    Offline support added

--- App.js
+++ App.js
@@ -4,14 +4,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
+import OfflineNotice from "./app/components/OfflineNotice";
 
 
 export default function App() {
 
   return (
-    <NavigationContainer theme={navigationTheme}>
-      {/* <AuthNavigator /> */}
-      <AppNavigator />
-    </NavigationContainer>
+    <>
+      <OfflineNotice />
+      <NavigationContainer theme={navigationTheme}>
+        {/* <AuthNavigator /> */}
+        <AppNavigator />
+      </NavigationContainer>
+    </>
   )
 }
